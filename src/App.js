cbb4739387ceb2ad46d3b41cc88d02a0977afe8a
deeparka <arkadeep180@gmail.com>
2022-02-24 01:18:47 +0530

    modified import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./components/Home";
 function App() {
     return (
         <div className="App">
-            {/* Navbar */}
             <Navbar />
             <div className="content">
                 <Home />
