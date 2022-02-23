1b1215066862c24711937d5f2029c9d45bab8b36
deeparka <arkadeep180@gmail.com>
2022-02-24 01:08:52 +0530

    created Home & Navbar js file and added in root App js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
     return (
         <div className="App">
+            {/* Navbar */}
+            <Navbar />
             <div className="content">
-                <h1>App</h1>
+                <Home />
             </div>
         </div>
     );
