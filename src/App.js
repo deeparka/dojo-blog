bf8b31172d9c093450f25aceed61c71adb491178
deeparka <arkadeep180@gmail.com>
2022-02-27 18:03:29 +0530

    added blogDetails.js that shows details of blogs using another route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Create from "./components/Create";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BlogDetails from "./components/BlogDetails";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Home />} />
                         <Route path="/create" element={<Create />} />
+                        <Route path="/blogs/:id" element={<BlogDetails />} />
                     </Routes>
                 </div>
             </div>
