36eae7b7616b1eed2ba8b46b3904eb7c62d6760c
deeparka <arkadeep180@gmail.com>
2022-02-28 13:09:50 +0530

    added post request so the new blogs can add to the database

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,12 +3,23 @@ import React, { useState } from "react";
 const Create = () => {
     const [title, setTitle] = useState(``);
     const [body, setBody] = useState(``);
-    const [author, setAuthor] = useState("mario");
+    const [author, setAuthor] = useState("Mario");
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
-        console.log(blog);
+        setIsPending(true);
+        // console.log(blog);
+
+        fetch("http://localhost:8000/blogs", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog),
+        }).then(() => {
+            console.log("New blog added!");
+            setIsPending(false);
+        });
     };
 
     return (
@@ -33,10 +44,11 @@ const Create = () => {
                     value={author}
                     onChange={(e) => setAuthor(e.target.value)}
                 >
-                    <option value="mario">mario</option>
-                    <option value="yoshi">yoshi</option>
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
             </form>
         </div>
     );
